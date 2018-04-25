488d80a5c30a203393c2234b17d8f1f0c25af0e5
Alejandro <alejandrombc@gmail.com>
2018-04-24 23:47:33 -0300

    Periodos actualizados con permisologia + limpieza de codigo

--- API SISGIU/API/periodo/permissions.py
+++ API SISGIU/API/periodo/permissions.py
@@ -12,13 +12,3 @@ class IsListOrCreate(BasePermission):
 			else:
 				return False
 		return True
-
-
-
-class isOwnerOrReadOnly(BasePermission):
-	message = "Debe ser el usuario iniciado para editar este objeto."
-	def has_object_permission(self, request, view, obj):
-		member = Usuario.objects.get(username=request.user)
-		if(member.is_superuser == True):
-			return True
-		return obj.usuario == request.user
